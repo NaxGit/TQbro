087f9f0fd2f2c73f0c9ced0bea78ab94d9a9bc23
NaxGit <145691347+NaxGit@users.noreply.github.com>
2023-12-20 19:18:33 +0300

    Add files via upload

---
+++ Oxygenated/keys.js
@@ -0,0 +1,48 @@
+const ddddss = 
+`
+<div id="alphabet">
+<div id="1" class="NumLock">1</div>
+<div id="2" class="NumLock">2</div>
+<div id="3" class="NumLock">3</div>
+<div id="4" class="NumLock">4</div>
+<div id="5" class="NumLock">5</div>
+<div id="6" class="NumLock">6</div>
+<div id="7" class="NumLock">7</div>
+<div id="8" class="NumLock">8</div>
+<div id="9" class="NumLock">9</div>
+<div id="0" class="NumLock">0</div>
+<div class="OSK">Q</div>
+<div class="OSK">W</div>
+<div class="OSK">E</div>
+<div class="OSK">R</div>
+<div class="OSK">T</div>
+<div class="OSK">Y</div>
+<div class="OSK">U</div>
+<div class="OSK">I</div>
+<div class="OSK">O</div>
+<div class="OSK">P</div>
+<div>&nbsp;</div>
+<div class="OSK">A</div>
+<div class="OSK">S</div>
+<div class="OSK">D</div>
+<div class="OSK">F</div>
+<div class="OSK">G</div>
+<div class="OSK">H</div>
+<div class="OSK">J</div>
+<div class="OSK">K</div>
+<div class="OSK">L</div>
+<div>&nbsp;</div>
+<div class="OSK">↑</div>
+<div class="OSK">Z</div>
+<div class="OSK">X</div>
+<div class="OSK">C</div>
+<div class="OSK">V</div>
+<div class="OSK">B</div>
+<div class="OSK">N</div>
+<div class="OSK">M</div>
+<div id="C" class="OSK NumLock">←</div>
+<div class="OSK">123</div>
+<div class="OSK">_</div>
+<div id="K" class="OSK NumLock">OK ✓</div>
+</div>
+`;
